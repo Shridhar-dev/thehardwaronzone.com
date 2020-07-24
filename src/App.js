c6341f1b0e9f4f9bf84c351769709c25651853e8
Shridhar-dev <shridharkamat10@gmail.com>
2020-07-24 19:33:14 +0530

    Clean Code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar.js';
+import Header from './Header.js';
+import About from './About.js';
+import Home from './Home.js';
+import {Switch , Route} from 'react-router-dom';
+import Projects from './Projects.js';
+import Blogs from './Blogs.js';
+import RecentProjects from './RecentProjects.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/projects' component={Projects} />
+        <Route exact path='/blogs' component={Blogs} />
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import ScrollToTop from './ScrollToTop';
+import {BrowserRouter} from 'react-router-dom';
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <ScrollToTop />
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>
+  ,
   document.getElementById('root')
 );
 
